e0a7a7faced48a91000bef23ff28ecfa8e1eff9f
Dorian <dorianpernot2@gmail.com>
2025-01-24 14:39:01 +0100

    ajout des services

---
+++ src/app/core/services/olympic.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Olympic } from '../models/Olympic';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OlympicService {
+  private olympicUrl = './assets/mock/olympic.json';
+  private olympics$ = new BehaviorSubject<Olympic[]>([]);
+
+  constructor(private http: HttpClient) {}
+
+  loadInitialData(): Observable<Olympic[]> {
+    return this.http.get<Olympic[]>(this.olympicUrl).pipe(
+      tap((olympics) => this.olympics$.next(olympics)),
+      catchError((error) => {
+        console.error('Error loading olympic data:', error);
+        this.olympics$.next([]);
+        throw error;
+      })
+    );
+  }
+
+  getOlympics(): Observable<Olympic[]> {
+    return this.olympics$.asObservable();
+  }
+
+  getTotalMedalsByCountry(olympics: Olympic[]): { country: string; medals: number }[] {
+    return olympics.map(olympic => ({
+      country: olympic.country,
+      medals: olympic.participations.reduce((total, p) => total + (p.medalsCount || 0), 0)
+    }));
+  }
+
+  getCountryDetails(countryId: number): Observable<Olympic | undefined> {
+    return this.olympics$.pipe(
+      map(olympics => olympics.find(olympic => olympic.id === countryId))
+    );
+  }
+}
